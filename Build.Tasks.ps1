483ce6b0b7280c9bbd68d3d709de63cc29a0aae3
Alex Ong <aong@thoughtworks.com>
2012-02-22 17:37:10 +1100

    moving pester invocation out to its own file...

--- Build.Tasks.ps1
+++ Build.Tasks.ps1
@@ -1,6 +1,3 @@
 task Test {
-    powershell -NonInteractive -NoProfile -ExecutionPolicy unrestricted -Command { Import-Module .\Pester.psm1; Invoke-Pester . }
-    if ($lastExitCode -ne 0) {
-        throw "Tests failed"
-    }
+    iex ".\runPester.ps1"
 }
